bbd77a48fe6f868e057099b8f709224e9bc82a53
SixtiFeverGit <jdswift94@gmail.com>
2026-07-29 05:21:38 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Nav } from './components/Nav'
 import { Hero } from './components/Hero'
 import { ProblemSolution } from './components/ProblemSolution'
-import { Differentiation } from './components/Differentiation'
 import { HowItWorks } from './components/HowItWorks'
 import { Benefits } from './components/Benefits'
 import { Features } from './components/Features'
